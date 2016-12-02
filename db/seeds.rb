029c6a63cf6f841ca6a39e47643503f9bfa73065
Alex <Alex@Kaisas-MacBook-Air.local>
2016-12-01 20:04:44 -0800

    We created a piece of code called seed that generates writing

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,13 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+name = ["First", "Second", "Third", "Forth"]
+name.each do |i|
+  episodes = Episode.create([{ name: "#{i}",
+                               description: "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you.",
+                               content: "Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit."}])
+end
+
+
+#    movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
